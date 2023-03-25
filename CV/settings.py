1b04811120ce1a736a623698020b423425cb3c06
Mohammad Javad Zare <mjz589.2018@gmail.com>
2023-03-26 03:08:38 +0330

    .env created for SECRET_KEY
    django and python-decouple are installed in venv.

--- CV/settings.py
+++ CV/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-69k$rucz#^dy4%hd-)ou@0r@ai@)sbac5d#zk7&gcz9v9r^e-x'
+from decouple import config
 
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
